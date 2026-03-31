d7020329669a65837a7e6e55a227b576cc0c807a
unknown <nguyenanhquoc077@gmail.com>
2026-03-31 08:34:54 +0700

    THUCHANH10_Deploy

--- server/index.js
+++ server/index.js
@@ -18,3 +18,18 @@ app.use('/api/admin', require('./api/admin.js'));
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+//deployment
+
+const path = require('path');
+// '/admin' serve the files at client-admin/build/* as static files
+app.use('/admin', express.static(path.join(__dirname, '../client-admin/build')));
+app.get('/admin/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client-admin/build', 'index.html'));
+});
+
+// '/customer' serve the files at client-customer/build/* as static files
+app.use('/customer', express.static(path.join(__dirname, '../client-customer/build')));
+app.get('/customer/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client-customer/build', 'index.html'));
+});
